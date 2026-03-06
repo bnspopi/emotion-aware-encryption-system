1cc50d40057af6fc7ec8d7449c6f65b146615795
bnspopi <sinhashounak@gmail.com>
2026-03-06 20:45:14 +0530

    Update encryption.py

--- backend/encryption.py
+++ backend/encryption.py
@@ -1,12 +1,13 @@
 from cryptography.fernet import Fernet
 
+# Generate key
 key = Fernet.generate_key()
 cipher = Fernet(key)
 
-def encrypt_text(text):
-    encrypted = cipher.encrypt(text.encode())
-    return encrypted
+def encrypt_message(message):
+    encrypted = cipher.encrypt(message.encode())
+    return encrypted.decode()
 
-def decrypt_text(encrypted_text):
-    decrypted = cipher.decrypt(encrypted_text).decode()
-    return decrypted
+def decrypt_message(encrypted_text):
+    decrypted = cipher.decrypt(encrypted_text.encode())
+    return decrypted.decode()
