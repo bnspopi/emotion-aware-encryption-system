4d44bf8adb66fdd1ba7bbbae555732f5242cb5af
bnspopi <sinhashounak@gmail.com>
2026-03-07 12:21:11 +0530

    Update emotion_detector.py

--- backend/emotion_detector.py
+++ backend/emotion_detector.py
@@ -1,26 +1,32 @@
 from transformers import pipeline
 
-emotion_pipeline = pipeline(
+emotion_model = pipeline(
     "text-classification",
     model="j-hartmann/emotion-english-distilroberta-base",
     top_k=None
 )
 
-
 def detect_emotion(text):
 
-    results = emotion_pipeline(text)[0]
+    results = emotion_model(text)[0]
 
     scores = {}
 
     for r in results:
         scores[r["label"]] = round(r["score"] * 100, 2)
 
-    detected_emotion = max(scores, key=scores.get)
-    confidence = scores[detected_emotion]
+    # sort emotions by score
+    sorted_emotions = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+
+    # top 2 emotions
+    top1 = sorted_emotions[0]
+    top2 = sorted_emotions[1]
+
+    emotion_text = f"{top1[0]} + {top2[0]}"
+    confidence_text = f"{top1[1]}% + {top2[1]}%"
 
     return {
-        "emotion": detected_emotion,
-        "confidence": confidence,
+        "emotion": emotion_text,
+        "confidence": confidence_text,
         "scores": scores
     }
